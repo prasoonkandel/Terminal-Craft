71f8ef5c9054e9afacffaa8661a3b99b5c29575a
Prasoon Kandel <prasoonkandel1029@gmail.com>
2025-12-30 20:10:02 +0545

    Added Rock Paper Scissors Game

--- main.cpp
+++ main.cpp
@@ -54,7 +54,65 @@ void playHeadOrTails() {
 
 
 void playRockPaperScissors() {
+     bool playAgain = true;
+    char choice;
+    int userChoice;
+    int computerChoice;
+
+    while (playAgain)
+    {
+        cout << "Choose Rock, Paper or Scissors (R/P/S): ";
+        cin >> choice;
+        choice = toupper(choice);
+        if (choice != 'R'&& choice != 'P' && choice != 'S') {
+            cout << "Invalid choice. Please choose R, P or S." << endl;
+            continue;
+        }
+        else {
+            if (choice == 'R') {
+                userChoice = 1;
+            } else if (choice == 'P') {
+                userChoice = 2;
+            } else {
+                userChoice = 3;
+            }
+        }
+        computerChoice = randInt(1, 3);
+
+        if (userChoice == computerChoice){
+            cout << "It's a tie!" << endl;
+        } else if (userChoice == 1 && computerChoice==3)
+        {
+            cout << "Rock crushes Scissors! You win!" << endl;
+
+        }else if (userChoice == 2 && computerChoice==1)
+        {
+            cout << "Paper covers Rock! You win!" << endl;
+
+        }else if (userChoice == 3 && computerChoice==2)
+        {
+       
+            cout << "Scissors cuts Paper! You win!" << endl;
+               } else if (computerChoice == 1 && userChoice==3)
+        {
+            cout << "Rock crushes Scissors! You lose!" << endl;
+
+        }else if (computerChoice == 2 && userChoice==1)
+        {
+            cout << "Rock gets covered by Paper! You lose!" << endl;
+
+        }else if (computerChoice == 3 && userChoice==2)
+        {
+             cout << "Paper gets cut by Scissors! You lose!" << endl;
+       }
     
+        cout << "Do you want to play again? (Y/N): ";
+        char again;
+        cin >> again;
+        if (toupper(again) != 'Y') {
+            playAgain = false;
+        }
+    }
 }
 
 int main(){
@@ -81,7 +139,7 @@ cout << "#====================================================#" << endl << endl
             break;
         case 2:
             cout << "You selected Rock Paper Scissors!" << endl;
-           
+            playRockPaperScissors();
             break;
         case 3:
             cout << "This game is coming soon!" << endl;
